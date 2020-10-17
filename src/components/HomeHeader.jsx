e60125b09be5ac6537b38e836866037e4c1c8d03
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-10-17 15:54:18 -0400

    home header

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,21 +1,72 @@
 import React from 'react'
-import ashlea from '../assets/ashlea-home.png'
+import styled from 'styled-components'
 
+import ashlea from '../assets/ashlea1.png'
+
+const Main = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`
+const LeftArea = styled.div`
+  background-color: rgb(14,24,84);
+  font-family: 'Manrope', sans-serif;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Center = styled.section`
+  width: 350px;
+`
+const Titles = styled.section`
+  text-align: left;
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 1.35em;
+  color: rgb(233,115,40)
+`
+const BtnSection = styled.section`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin: 15px 0
+
+`
+const Btn = styled.button`
+  padding: 6px;
+  width: 100px;
+  background-color: rgb(233,115,40);
+  border: rgb(233,115,40);
+  border-radius: 5px;
+  box-shadow: 5px 5px 0px rgb(233,115,40)
+
+
+`
+
+const Image = styled.img`
+  width: 50%;
+`
 
 export default function HomeHeader() {
   return (
-    <div>
-      <div>
-        <h2>Neuroscientist.</h2>
-        <h2>PH.D Candidate.</h2>
-        <h2>Writer.</h2>
-        <section>
-          <button>What I Do</button>
-          <button>Blog</button>
-          <button>Contact Me</button> 
-        </section>
-      </div>
-      <img src={ashlea} alt=""/>
-    </div>
+    <Main>
+      <LeftArea>
+        <Center>
+          <Titles>
+            <h2>Neuroscientist.</h2>
+            <h2>PH.D Candidate.</h2>
+            <h2>Writer.</h2>
+          </Titles>
+          <BtnSection>
+            <Btn>What I Do</Btn>
+            <Btn>Blog</Btn>
+            <Btn>Contact Me</Btn>
+          </BtnSection>
+        </Center>
+      </LeftArea>
+      <Image src={ashlea} alt="" />
+    </Main>
   )
 }
