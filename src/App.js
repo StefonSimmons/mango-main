cefbccfef73ed2bf890b05375726f4faeeefc30c
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-10-17 20:07:59 -0400

    workido added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Header from './components/Header'
 import HomeHeader from './components/HomeHeader'
 import About from './components/About'
 import WhatIDo from './components/WhatIDo'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div>
       <Header />
+
       <Route exact path='/'>
         <HomeHeader />
         <About />
         <WhatIDo />
       </Route>
+
       <Route path='/writing'>
         <h1>Hi there</h1>
       </Route>
+
+      <Footer/>
     </div>
   );
 }
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -10,36 +10,72 @@ import researchImg from '../assets/research.png'
 const WorkContainer = styled.div`
   display: flex;
   flex-direction: row;
-  // border: red solid 2px;
 `
 const Work = styled.section`
   height: 500px;
   width: 33.33%;
-  // border: green 4px solid
+  display: flex;
+  align-items: flex-end;
 `
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50%;
+  width: 100%;
+  background: rgba(14,24,84,.35);
+  font-family: 'Manrope', sans-serif;
+  font-weight: 700;
+`
+const Title = styled.h2`
+  color: rgb(233,115,40);
+  background: rgba(14,24,84,.2);
+  box-shadow: 2px 2px 10px 5px rgba(14,24,84,.20);
+  border-radius: 5px;
+  padding: 5px;
+  margin-bottom: 50px;
+  font-size: 24px;
+`
+const Btn = styled.button`
+  background-color: rgba(0,0,0,0);
+  color: rgb(233,115,40);
+  color: white;
+  font-size: 18px;
+  border: rgb(233,115,40) 1px solid;
+  padding: 15px 30px;
+  transition: all .3s ease-in-out;
 
-// additional styles
-const writing = {
-  backgroundImage: `url(${writingImg})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
-}
-const outreach = {
-  backgroundImage: `url(${outreachImg})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
+  &:hover{
+    box-shadow: 5px 5px 0px rgb(233,115,40);
+    background-color: rgba(255,135,60,.4);
+    transform: scale(1.05);
+    border: rgba(255,135,60,.1) 1px solid;
 
-}
-const research = {
-  backgroundImage: `url(${researchImg})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
+  }
 
-}
-
+`
 
 export default function WhatIDo() {
 
+  // background image styles
+  const writing = {
+    backgroundImage: `url(${writingImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+  const outreach = {
+    backgroundImage: `url(${outreachImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+
+  }
+  const research = {
+    backgroundImage: `url(${researchImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+
+  }
   const iDoArr = [
     {
       text: 'writing',
@@ -55,17 +91,18 @@ export default function WhatIDo() {
     }
   ]
   const iDo = iDoArr.map(work => {
+    const formatedText = work.text.charAt(0).toUpperCase().concat(work.text.slice(1)) //proper case
     return (
       <Work style={work.style}>
 
-        <div>
+        <Box>
 
-          <h3>{work.text}</h3>
+          <Title>{formatedText}</Title>
           <Link to='/writing'>
-            <button>Go to</button>
+            <Btn>Go to</Btn>
           </Link>
-        
-        </div>
+
+        </Box>
 
       </Work>
     )
