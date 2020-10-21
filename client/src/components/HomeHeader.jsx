df7f9d3a2bdb55d1079dc8de4c5846279eefedb0
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-10-21 02:06:21 -0400

    smaller Home component

--- client/src/components/HomeHeader.jsx
+++ client/src/components/HomeHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavBar from './NavBar'
 import styled, { keyframes } from 'styled-components'
 
 import ashlea from '../assets/ashlea1.WebP'
@@ -7,11 +8,6 @@ import ashlea from '../assets/ashlea1.WebP'
 export const fadeIn = keyframes`
   0%{opacity: 0}
   100%{opactiy: 1}
-`
-const slideIn = keyframes`
-  0%{bottom: -10%; opacity: 0; border-bottom: ${({theme})=> theme.blue} solid 1px}
-  50%{bottom: -5%; opacity: .5; border-bottom: ${({theme})=> theme.blue} solid 1px}
-  100%{bottom: 0%; opacity: 1}
 `
 
 
@@ -46,41 +42,6 @@ const Titles = styled.section`
 const Title = styled.h2`
   animation: ${fadeIn} 1.5s ease-in-out;
 `
-const NavBar = styled.nav`
-  height: 100px;
-  width: 100vw;
-  padding: 20px 0;
-  background-color: ${({theme})=> theme.blue};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-const BtnSection = styled.section`
-  display: flex;
-  justify-content: space-around;
-  width: 50%;
-
-  position: relative;
-  animation: ${slideIn} 1.5s linear;
-`
-const Btn = styled.button`
-  padding: 12px;
-  width: 200px;
-  background-color: ${({theme})=> theme.orange};
-  color: ${({theme})=> theme.blue};
-  border: ${({theme})=> theme.orange} solid 1px;
-  transition: all .3s ease-in-out;
-  font-size: 24px;
-
-  &:hover{
-    transform: scale(1.05);
-    background-color: rgb(255,135,60);
-    color: black;
-    box-shadow: 10px 10px 0px rgb(213,95,20);
-    border-radius: 5px;
-    font-weight: 700;
-  }
-`
 const Image = styled.img`
   width: 50%;
 `
@@ -100,19 +61,7 @@ export default function HomeHeader() {
         </LeftArea>
         <Image src={ashlea} alt="ashlea" />
       </Main>
-      <NavBar>
-        <BtnSection>
-          <a href='#work'>
-            <Btn>What I Do</Btn>
-          </a>
-          <a href="https://mango-mosaic.netlify.app/blog" target='_blank' rel="noopener noreferrer">
-            <Btn>Blog</Btn>
-          </a>
-          <a href='#contact'>
-            <Btn>Contact Me</Btn>
-          </a>
-        </BtnSection>
-      </NavBar>
+      <NavBar/>
     </>
   )
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+
+// animation
+const slideUp = keyframes`
+  0%{bottom: -10%; opacity: 0; border-bottom: ${({theme})=> theme.blue} solid 1px}
+  50%{bottom: -5%; opacity: .5; border-bottom: ${({theme})=> theme.blue} solid 1px}
+  100%{bottom: 0%; opacity: 1}
+`
+
+const Main = styled.nav`
+  height: 100px;
+  width: 100vw;
+  padding: 20px 0;
+  background-color: ${({theme})=> theme.blue};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const BtnSection = styled.section`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+
+  position: relative;
+  animation: ${slideUp} 1.5s linear;
+`
+const Btn = styled.button`
+  padding: 12px;
+  width: 200px;
+  background-color: ${({theme})=> theme.orange};
+  color: ${({theme})=> theme.blue};
+  border: ${({theme})=> theme.orange} solid 1px;
+  transition: all .3s ease-in-out;
+  font-size: 24px;
+
+  &:hover{
+    transform: scale(1.05);
+    background-color: rgb(255,135,60);
+    color: black;
+    box-shadow: 10px 10px 0px rgb(213,95,20);
+    border-radius: 5px;
+    font-weight: 700;
+  }
+`
+
+export default function NavBar() {
+  return (
+    <Main>
+      <BtnSection>
+        <a href='#work'>
+          <Btn>What I Do</Btn>
+        </a>
+        <a href="https://mango-mosaic.netlify.app/blog" target='_blank' rel="noopener noreferrer">
+          <Btn>Blog</Btn>
+        </a>
+        <a href='#contact'>
+          <Btn>Contact Me</Btn>
+        </a>
+      </BtnSection>
+    </Main>
+  )
+}
